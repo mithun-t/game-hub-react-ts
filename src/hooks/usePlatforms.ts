e769fb61fd12cfdc8972dd74b053ba634ea4bdd2
Happy Tom <mithunaksh01@gmail.com>
2024-01-02 22:36:46 +0530

    Build a platform selector

---
+++ src/hooks/usePlatforms.ts
@@ -0,0 +1,14 @@
+import useData from "./useData";
+
+interface Platform{
+    id:number;
+    name:string;
+    slug:string;
+}
+
+  const usePlatforms = () => {
+    const { data, error, isLoading } = useData<Platform>('/platforms/lists/parents');
+    return { data, error, isLoading };
+};
+
+export default usePlatforms;
