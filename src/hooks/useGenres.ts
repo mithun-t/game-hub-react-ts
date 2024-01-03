5f43bac4f011da227a6584cb83ef769e2870628d
Happy Tom <mithunaksh01@gmail.com>
2024-01-04 00:09:25 +0530

    ship genres with app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,8 +1,8 @@
-import useData from "./useData";
+import genres from "../data/genres";
 export interface Genre {
     id: number;
     name: string;
     image_background:string;
   }
-const useGames=()=> useData<Genre>("/genres");
-export default useGames;
+const useGenres=()=> ({data:genres,isLoading:false,error:null})
+export default useGenres;
